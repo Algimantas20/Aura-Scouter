f219f8291de558648da8bbb0ac3319e35f318977
Algimantas20 <algsamalionis@gmail.com>
2024-06-02 15:10:36 +0300

    first commit

---
+++ source/register-commands.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+const { ApplicationCommandOptionType, REST, Routes } = require("discord.js");
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+const commands = [
+  {
+    name: "aura-count",
+    description: "Add or remove aura to a user",
+    options: [
+      {
+        name: "number",
+        description: "How much do you want to add or remove",
+        type: ApplicationCommandOptionType.Number,
+        required: true,
+      },
+      {
+        name: "username",
+        description: "The username of the person you want to mute",
+        type: ApplicationCommandOptionType.User,
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "aura-reveal",
+    description: "Shows everyones aura",
+  },
+  {
+    name: "aura-clear",
+    description: "Clears everyones aura to zero",
+  },
+];
+
+(async () => {
+  try {
+    console.log("Registering slash commands...");
+
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: commands }
+    );
+
+    console.log("Slash commands were registered successfully!");
+  } catch (error) {
+    console.error(`There was an error: ${error.message}`);
+  }
+})();
